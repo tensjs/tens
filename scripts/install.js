cc394047739520c6642b9d71e9d48782ea8b5d83
xiaoyu <306766053@qq.com>
2017-08-04 16:35:14 +0800

    format code

--- scripts/install.js
+++ scripts/install.js
@@ -3,32 +3,32 @@
  * download tensorflow
  */
 
- const pkg = require('../package.json');
- const os = require('os');
- const urllib = require('urllib');
- const compressing = require('compressing');
- const path = require('path');
+const pkg = require('../package.json');
+const os = require('os');
+const urllib = require('urllib');
+const compressing = require('compressing');
+const path = require('path');
 
- const type = 'cpu';//TODO: support other type
+const type = 'cpu';// TODO: support other type
 
- const prefix = 'https://storage.googleapis.com/tensorflow/libtensorflow';
- const libtensorflow = `${prefix}/libtensorflow-${type}-${os.platform()}-x86_64-${pkg.tensorflow}.tar.gz`;
- const libtensorflow_proto = `${prefix}/libtensorflow_proto-${pkg.tensorflow}.zip`;
+const prefix = 'https://storage.googleapis.com/tensorflow/libtensorflow';
+const libtensorflow = `${prefix}/libtensorflow-${type}-${os.platform()}-x86_64-${pkg.tensorflow}.tar.gz`;
+const libtensorflow_proto = `${prefix}/libtensorflow_proto-${pkg.tensorflow}.zip`;
 
 // download libtensorflow_proto
 urllib.request(libtensorflow_proto, {
   streaming: true,
   followRedirect: true,
 })
-.then(result => compressing.zip.uncompress(result.res, path.join(__dirname, '..')))
-.then(() => console.log('libtensorflow_proto is downloaded'))
-.catch(console.error);
+  .then(result => compressing.zip.uncompress(result.res, path.join(__dirname, '..')))
+  .then(() => console.log('libtensorflow_proto is downloaded'))
+  .catch(console.error);
 
 // download libtensorflow
 urllib.request(libtensorflow, {
   streaming: true,
   followRedirect: true,
 })
-.then(result => compressing.tgz.uncompress(result.res, path.join(__dirname, '../tensorflow')))
-.then(() => console.log('libtensorflow is downloaded'))
-.catch(console.error);
+  .then(result => compressing.tgz.uncompress(result.res, path.join(__dirname, '../tensorflow')))
+  .then(() => console.log('libtensorflow is downloaded'))
+  .catch(console.error);
