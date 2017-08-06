8bf9982ec808496d58a58946cfb454fa7bea0258
zihan-eric <zhangyongjun369@gmail.com>
2017-08-07 01:51:30 +0800

    fix test and eslint

--- test/graph.test.js
+++ test/graph.test.js
@@ -1,7 +1,6 @@
 const tf = require('../');
 
-/* eslint no-underscore-dangle: ["error", { "allow": ["__version"] }] */
 test('graph', () => {
-  const graph = tf.Graph();
-  expect(true).toBe(true);
+  const graph = new tf.Graph();
+  expect(graph instanceof tf.Graph).toBe(true);
 });
--- test/operation.test.js
+++ test/operation.test.js
@@ -1,9 +1,8 @@
 const tf = require('../');
 
-/* eslint no-underscore-dangle: ["error", { "allow": ["__version"] }] */
 test('operation', () => {
   const graph = tf.Graph();
-  const oper = tf.Operation(graph, "Const", "const_op");
+  const oper = tf.Operation(graph, 'Const', 'c');
   oper.finish();
 
   expect(oper.graph === graph).toBe(true);
