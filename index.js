f9748ded87ea3804d7ff299105b0ac3c145c5549
仔寒 <zihan.zyj@alibaba-inc.com>
2017-08-07 01:51:30 +0800

    fix test and eslint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tf = require('bindings')('tensorflow');
 
 module.exports = {
+  Graph: tf.Graph,
+  Operation: tf.Operation,
   __version: tf.version(),
 };
-
--- scripts/gen_ops.js
+++ scripts/gen_ops.js
@@ -1,11 +1,14 @@
-const os = require("os");
 const path = require('path');
 const protobuf = require('protobufjs');
 const tf = require('bindings')('tensorflow');
 
-const root = protobuf.Root.fromJSON(require("../op_def.json"));
+// eslint-disable-next-line import/no-dynamic-require
+const json = require(path.join(__dirname, '../op_def.json'));
+const root = protobuf.Root.fromJSON(json);
 
 const OpListMessage = root.lookupType('OpList');
+
+// eslint-disable-next-line no-underscore-dangle
 const oplist = OpListMessage.decode(tf._getAllOpList()).op;
 
 console.log(oplist);
